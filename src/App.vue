b93ba85826491153583cccb62502cf47922e1e08
Ahmet Ordulu <ahmet.ordulu@hotmail.se>
2022-02-02 17:16:08 +0100

    updated header and hero

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
-<div>
-  <Header />
-  <main>
-    <Hero />
-  </main>
-
-</div>
+  <div>
+    <Header />
+    <main>
+      <Hero />
+    </main>
+  </div>
 </template>
 
 <script>
--- src/components/Hero/Hero.vue
+++ src/components/Hero/Hero.vue
@@ -3,14 +3,21 @@
     <video class="absolute object-cover inset-0 h-full w-full overflow-hidden -z-1" src="../../assets/Video/Moving-bg.mp4" loop autoplay playsinline muted></video>
     <div class="absolute top-2/4 left-2/4 text-center transform -translate-x-2/4 -translate-y-2/4">
       <h1>
-        Test
+        Mainset Music
       </h1>
-      <div>
-        <a href="#">link 1</a>
-        <a href="#">link 2</a>
-        <a href="#">link 3</a>
-        <a href="#">link 4</a>
-        <a href="#">link 5</a>
+      <div class="flex justify-between">
+        <div class="p-10 bg-black text-white">
+          <a href="#">Shop</a>
+        </div>
+        <div>
+          <a href="#">Youtube</a>
+        </div>
+        <div>
+          <a href="#">Instagram</a>
+        </div>
+        <div>
+          <a href="#">Podcast</a>
+        </div>
       </div>
     </div>
   </section>
