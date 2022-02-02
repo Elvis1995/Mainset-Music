9b5c9867f08c5baa38dd18d4b04fa0a996671a6e
Ahmet Ordulu <ahmet.ordulu@hotmail.se>
2022-02-02 17:23:26 +0100

    added source tag for semantic html

--- src/components/Hero/Hero.vue
+++ src/components/Hero/Hero.vue
@@ -1,6 +1,8 @@
 <template>
   <section class="relative h-750">
-    <video class="absolute object-cover inset-0 h-full w-full overflow-hidden -z-1" src="../../assets/Video/Moving-bg.mp4" loop autoplay playsinline muted></video>
+    <video class="absolute object-cover inset-0 h-full w-full overflow-hidden -z-1" loop autoplay playsinline muted>
+      <source src="../../assets/Video/Moving-bg.mp4" type="video/mp4">
+    </video>
     <div class="absolute top-2/4 left-2/4 text-center transform -translate-x-2/4 -translate-y-2/4">
       <h1>
         Mainset Music
