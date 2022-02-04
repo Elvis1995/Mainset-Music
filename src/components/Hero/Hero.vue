9e89e6e22d9d8baa836f72c86edc9e76a30aca2d
Ahmet Ordulu <ahmet.ordulu@hotmail.se>
2022-02-04 12:36:53 +0100

    formatted code

--- src/components/Hero/Hero.vue
+++ src/components/Hero/Hero.vue
@@ -1,36 +1,31 @@
 <template>
-  <section class="relative h-750">
-    <video class="absolute inset-0 object-cover w-full h-full overflow-hidden -z-1" loop autoplay playsinline muted>
-      <source src="https://themes.fourthwall.com/themes/assets/11d2f253-34b7-4fdc-b01b-17868cd74972/assets/video-bg-light.mp4?v=364d6351-2cdd-8622-b962-d429a0989974" type="video/mp4">
-    </video>
-    <div class="absolute text-center transform top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4">
-      <h1>
-        Mainset Music
-      </h1>
-      <div class="flex justify-between">
-        <div class="p-10 text-white bg-black">
-          <a href="#">Shop</a>
-        </div>
-        <div>
-          <a href="#">Youtube</a>
-        </div>
-        <div>
-          <a href="#">Instagram</a>
-        </div>
-        <div>
-          <a href="#">Podcast</a>
-        </div>
-      </div>
-    </div>
-  </section>
+	<section class="relative h-750">
+		<video class="absolute inset-0 object-cover w-full h-full overflow-hidden -z-1" loop autoplay playsinline muted>
+			<source
+				src="https://themes.fourthwall.com/themes/assets/11d2f253-34b7-4fdc-b01b-17868cd74972/assets/video-bg-light.mp4?v=364d6351-2cdd-8622-b962-d429a0989974"
+				type="video/mp4"
+			/>
+		</video>
+		<div class="absolute text-center transform top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4">
+			<h1>Mainset Music</h1>
+			<div class="flex justify-between">
+				<div class="p-10 text-white bg-black">
+					<a href="#">Shop</a>
+				</div>
+				<div>
+					<a href="#">Youtube</a>
+				</div>
+				<div>
+					<a href="#">Instagram</a>
+				</div>
+				<div>
+					<a href="#">Podcast</a>
+				</div>
+			</div>
+		</div>
+	</section>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
-
-<style>
-
-</style>
